d50940198932f3f642d8a2bdd3811c117c068068
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2019-09-01 19:06:05 +0200

    cicd

--- backend/test/status.js
+++ backend/test/status.js
@@ -5,16 +5,15 @@
 
 const mochaPlugin = require('serverless-mocha-plugin');
 const expect = mochaPlugin.chai.expect;
-let wrapped = mochaPlugin.getWrapper('status', '/src/status/index.js', 'handler');
+let status = mochaPlugin.getWrapper('status', '/src/status/index.js', 'handler');
 
 describe('status', () => {
-  before((done) => {
-    done();
-  });
-
-  it('implement tests here', () => {
-    return wrapped.run({}).then((response) => {
-      expect(response).to.not.be.empty;
+    before((done) => {
+        done();
     });
-  });
+
+    it('implement tests here', () => {
+        return status.run({})
+            .then((response) => expect(response).to.not.be.empty);
+    });
 });
