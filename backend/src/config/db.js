a7c0e300a7113e6c9b2fb61a24690cfde0069b59
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2019-09-03 23:31:53 +0200

    created crud with integration tests

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,26 @@
 'use strict';
-
+const MongoClient = require('mongodb').MongoClient;
 const DB_NAME = process.env.DB_NAME;
 const mongoUser = process.env.DB_USER;
 const mongoPass = process.env.DB_PASSWORD;
 const uri = `mongodb+srv://${mongoUser}:${mongoPass}@flashcards-ldxf4.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
+let cachedDb = null;
+
+const connectToDatabase  = (uri)  => {
+    if (cachedDb) {
+        return Promise.resolve(cachedDb);
+    }
+
+    return MongoClient.connect(uri)
+        .then(db => {
+            cachedDb = db;
+            return cachedDb;
+        });
+};
 
 module.exports = {
+    connectToDatabase,
     uri,
     DB_NAME
 };
--- backend/src/controller/flashcard.js
+++ backend/src/controller/flashcard.js
@@ -1,18 +1,114 @@
 'use strict';
-const MongoClient = require('mongodb').MongoClient;
-const {uri, mongoDB} = require('../config/db');
-const client = new MongoClient(uri, {useNewUrlParser: true});
-const {response} = require('../service/generic');
-const {SUCCESSFUL_RESPONSE, INTERNAL_SERVER_ERROR} = require('../constants/statusCodes');
+const {uri, mongoDB, connectToDatabase} = require('../config/db');
+const {response, validateInput, emptyFieldsError, extractId, isIdParameter, extractObjectId, skipId} = require('../service/generic');
+const {SUCCESSFUL_RESPONSE, INTERNAL_SERVER_ERROR, UNPROCESSABLE_ENTITY, NOT_FOUND} = require('../constants/statusCodes');
 const {FLASHCARDS} = require('../constants/collections');
 
-module.exports.save = (event, context, callback) => {
+const save = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
     const data = JSON.parse(event.body);
-    client.connect()
-        .then((client, err) =>
-            client.db(mongoDB).collection(FLASHCARDS).insertOne(data))
+    const inputErrors = validateInput(data);
+
+    if (Array.isArray(inputErrors) && inputErrors.length) {
+        callback(null, emptyFieldsError(inputErrors));
+        return;
+    }
+
+    connectToDatabase(uri)
+        .then((client, err) => client.db(mongoDB).collection(FLASHCARDS).insertOne(data))
         .then((result) =>
-            callback(null, response(SUCCESSFUL_RESPONSE, {message: 'Flashcard saved', flashcard: result.ops})))
+            callback(null, response(SUCCESSFUL_RESPONSE, {message: 'Flashcard saved', _id: result.insertedId})))
         .catch(err => callback(response(INTERNAL_SERVER_ERROR, err)))
-        .finally(() => client.close());
+};
+
+const remove = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    const data = JSON.parse(event.body);
+    const inputErrors = validateInput(data);
+
+    if (!isIdParameter(data)) {
+        callback(null, response(UNPROCESSABLE_ENTITY, {message: "Input should contain only _id"}));
+        return;
+    }
+
+    if (Array.isArray(inputErrors) && inputErrors.length) {
+        callback(null, emptyFieldsError(inputErrors));
+        return;
+    }
+
+    connectToDatabase(uri)
+        .then((client, err) => client.db(mongoDB).collection(FLASHCARDS).deleteOne(extractObjectId(data)))
+        .then((result) =>
+            result.deletedCount !== 0 ?
+                callback(null, response(SUCCESSFUL_RESPONSE, {message: 'Flashcard deleted', ...extractId(data)})) :
+                callback(null, response(NOT_FOUND, {message: `Flashcard with _id: ${data._id} does not exist`})))
+        .catch(err => callback(response(INTERNAL_SERVER_ERROR, err)))
+};
+
+const update = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    const data = JSON.parse(event.body);
+    const inputErrors = validateInput(data);
+
+    if (Array.isArray(inputErrors) && inputErrors.length) {
+        callback(null,  emptyFieldsError(inputErrors));
+        return;
+    }
+
+    if (!isIdParameter(data)) {
+        callback(null, response(UNPROCESSABLE_ENTITY, {message: "Input should contain _id"}));
+        return;
+    }
+
+     connectToDatabase(uri)
+        .then((client, err) => client.db(mongoDB).collection(FLASHCARDS).updateOne(extractObjectId(data), {$set: skipId(data)}))
+        .then((result) =>
+             result.matchedCount === 1 ?
+                callback(null, response(SUCCESSFUL_RESPONSE, {message: 'Flashcard updated', flashcard: data})) :
+                callback(null, response(NOT_FOUND, {message: `Flashcard with _id: ${data._id} does not exist`})))
+        .catch(err => callback(response(INTERNAL_SERVER_ERROR, err)))
+};
+
+const getAll = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    connectToDatabase(uri)
+        .then((client, err) => client.db(mongoDB).collection(FLASHCARDS).find({}).toArray())
+        .then((result) =>
+            callback(null, response(SUCCESSFUL_RESPONSE, {message: 'Flashcards', flashcard: result})))
+        .catch(err => callback(response(INTERNAL_SERVER_ERROR, err)))
+};
+
+const getBy = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    const stringParameters = event.queryStringParameters;
+
+    if (stringParameters === undefined) {
+        callback(null, response(UNPROCESSABLE_ENTITY, {message: 'Missing query string parameter'}));
+        return;
+    }
+
+    if (Object.keys(stringParameters).length > 1) {
+        callback(null, response(UNPROCESSABLE_ENTITY, {message: 'You can pass only 1 query string parameter'}));
+        return;
+    }
+
+    connectToDatabase(uri)
+        .then((client, err) =>
+            isIdParameter(stringParameters) ?
+                client.db(mongoDB).collection(FLASHCARDS).find(extractObjectId(stringParameters)).toArray() :
+                client.db(mongoDB).collection(FLASHCARDS).find(stringParameters).toArray())
+        .then((result) =>
+            result.length !== 0 ?
+                callback(null, response(SUCCESSFUL_RESPONSE, {message: 'Query result', flashcard: result})) :
+                callback(null, response(NOT_FOUND, {message: 'No flashcards were found', flashcard: result})))
+        .catch(err => callback(response(INTERNAL_SERVER_ERROR, err)))
+};
+
+module.exports = {
+    getAll,
+    save,
+    update,
+    remove,
+    getBy,
 };
